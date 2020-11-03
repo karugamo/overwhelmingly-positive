2d514fef7e3ee2572ee072d009fa15dff3fc4c99
Finn Pauls <mbp@pauls.dev>
2020-11-03 16:58:23 +0000

    Show some game links

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
+import games from '../games.json'
 
 export default function App() {
-  return <Main>a nice new project</Main>
+  return (
+    <Main>
+      {games.map((game) => (
+        <Game key={game.appId} {...game} />
+      ))}
+    </Main>
+  )
+}
+
+function Game({appId, positive, negative, name}) {
+  return (
+    <GameContainer>
+      <GameLink
+        href={`https://store.steampowered.com/app/${appId}`}
+        target="_blank"
+      >
+        {name}
+      </GameLink>
+    </GameContainer>
+  )
 }
 
 const Main = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100%;
 
   background-color: #eeeee4;
   color: #242422;
-  font-size: 75px;
+  font-size: 30px;
 `
+
+const GameLink = styled.a``
+
+const GameContainer = styled.div``
