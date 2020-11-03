08ef70d8beec09950d928346811e50a683a7a0be
Finn Pauls <derfinn@gmail.com>
2020-11-03 15:47:18 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# react-template
+
+Run development server with
+```
+npm start
+```
+
+In `package.json` replace
+```
+    "build": "parcel build index.html --public-url '/react-template'"
+```
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function App() {
+  return <Main>a nice new project</Main>
+}
+
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  background-color: #eeeee4;
+  color: #242422;
+  font-size: 75px;
+`
