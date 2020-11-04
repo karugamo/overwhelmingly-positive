86f5d5c551ae41f12ea6ff550c0a70cc4245557a
Finn Pauls <mbp@pauls.dev>
2020-11-04 14:49:06 +0000

    Some cleanup for gatsby

---
+++ src/components/Head.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Helmet} from 'react-helmet'
+
+export default function Head() {
+  return (
+    <Helmet>
+      <title>Overwhelmingly Positive Rated Games on Steam</title>
+      <meta
+        name="description"
+        content="See overwhelmingly positive rated games on steam in a list"
+      />
+
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, minimum-scale=1"
+      />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-1R0QR3M2YR"
+      ></script>
+      <script>
+        {`
+       window.dataLayer = window.dataLayer || [];
+       function gtag(){dataLayer.push(arguments);}
+       gtag('js', new Date());
+  
+       gtag('config', 'G-1R0QR3M2YR');
+  `}
+      </script>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Helmet>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import games from '../../games.json'
-import {Helmet} from 'react-helmet'
 import {shuffle} from 'lodash'
+import Head from '../components/Head'
 
 import '../styles/main.css'
 
@@ -36,54 +36,6 @@ function Game({appId, name}) {
         />
       </GameLink>
     </GameContainer>
-  )
-}
-
-function Head() {
-  return (
-    <Helmet>
-      <title>Overwhelmingly Positive Rated Games on Steam</title>
-      <meta
-        name="description"
-        content="See overwhelmingly positive rated games on steam in a list"
-      />
-
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, minimum-scale=1"
-      />
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-1R0QR3M2YR"
-      ></script>
-      <script>
-        {`
-     window.dataLayer = window.dataLayer || [];
-     function gtag(){dataLayer.push(arguments);}
-     gtag('js', new Date());
-
-     gtag('config', 'G-1R0QR3M2YR');
-`}
-      </script>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/static/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/static/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/static/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/static/site.webmanifest" />
-    </Helmet>
   )
 }
 
