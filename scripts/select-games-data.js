bf4cd590083f50d435d0d76465c594204e7b5144
Finn Pauls <mbp@pauls.dev>
2020-11-11 17:28:56 +0000

    Adjust selecting the g2a game to our own rating

--- scripts/select-games-data.js
+++ scripts/select-games-data.js
@@ -22,7 +22,7 @@ const games = selectedGames.map(rawGameToGame)
 console.log(createEnum(genres, 'Genre'))
 console.log(createEnum(categories, 'Category'))
 
-const json = JSON.stringify(games)
+const json = JSON.stringify(games, null, ' ')
 fs.writeFileSync('./data/games.json', json)
 
 function rawGameToGame(rawGame) {
@@ -57,12 +57,19 @@ function getG2a(appId) {
   const listing = g2a[appId]
   if (!listing) return false
 
-  if (!listing.offers) {
+  const offers = listing.offers
+
+  if (!offers) {
     console.error(listing.slug)
     window.process.exit()
   }
 
-  const offer = listing.offers.find(({selectedOffer}) => selectedOffer)
+  const offer = selectOffer(offers)
+
+  // console.log()
+  // console.log(listing.slug)
+  // console.log(offers.map(debugFormatOffer).join('\n'))
+  // console.log(offer.price.value)
 
   if (!offer) return false
 
@@ -71,9 +78,53 @@ function getG2a(appId) {
     price: offer?.price?.value,
     currency: offer?.price?.currency,
     sellerName: offer?.customer?.name,
-    sellerVotes: _.sum(Object.values(offer?.customer?.votes)),
+    sellerVotes: getTotalVotes(offer),
     sellerRating: offer?.customer?.rating
   }
+}
+
+function getTotalVotes(offer) {
+  return _.sum(Object.values(offer?.customer?.votes))
+}
+
+function debugFormatOffer(offer) {
+  const totalRatings = getTotalVotes(offer)
+
+  const rating = offer.customer.rating
+
+  const adjustedRating = getAdjustedRating(offer)
+
+  return `${offer.price.value} ${totalRatings}  ${rating} ${adjustedRating}`
+}
+
+function selectOffer(offers) {
+  return _.sortBy(offers, (offer) => {
+    const adjustedRating = getAdjustedRating(offer)
+    const pricePenalty = calcPricePenalty(adjustedRating)
+
+    return offer.price.value + pricePenalty
+  })[0]
+}
+
+function calcPricePenalty(adjustedRating) {
+  if (adjustedRating > 97) return 0
+
+  const power = 0.999
+  const multiply = 20
+
+  const max = 100 / 100 ** power
+  const raw = multiply * (100 / adjustedRating ** power - max)
+
+  return Math.round(raw * 100) / 100
+}
+
+function getAdjustedRating(offer) {
+  const totalRatings = _.sum(Object.values(offer.customer.votes))
+
+  const rating = offer.customer.rating
+  return Math.round(
+    rating - (rating - 0.5) * Math.pow(4, -Math.log10(totalRatings + 1))
+  )
 }
 
 function getCategoryIdMap(games) {
