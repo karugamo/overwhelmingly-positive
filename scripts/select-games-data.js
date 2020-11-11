9e6506ad540710ede62634dd67cb41fb5e300eaf
Finn Pauls <mbp@pauls.dev>
2020-11-11 15:55:52 +0000

    Refactor select-games-data into functions

--- scripts/select-games-data.js
+++ scripts/select-games-data.js
@@ -9,46 +9,35 @@ const relevantCategories = [2, 1, 9, 31]
 const selectedGames = rawGames.filter(({type}) => type === 'game')
 console.log('Removed', rawGames.length - selectedGames.length, 'non-games')
 
-console.log('categories map')
-const categories = _.mapValues(
-  _.keyBy(
-    _.flatten(selectedGames.map(({categories}) => categories)).filter(({id}) =>
-      relevantCategories.includes(id)
-    ),
-    'id'
-  ),
-  'description'
-)
+console.log('getCategoryIdMap')
+const categories = getCategoryIdMap(selectedGames)
 console.log(categories)
 
-console.log('genres map')
-const genres = _.mapValues(
-  _.keyBy(
-    _.flatten(selectedGames.map(({genres}) => genres)).filter((a) => a),
-    'id'
-  ),
-  'description'
-)
-
+console.log('getGenreIdMap')
+const genres = getGenreIdMap(selectedGames)
 console.log(genres)
 
-const games = selectedGames.map((rawGame) => ({
-  name: rawGame.name,
-  appId: rawGame.appId,
-  video: rawGame.video?.id,
-  categories:
-    rawGame.categories
-      .filter(({id}) => relevantCategories.includes(id))
-      .map(({id}) => id) ?? [],
-  genres: rawGame.genres?.map(({id}) => Number(id)) ?? [],
-  g2a: getG2a(rawGame.appId)
-}))
+const games = selectedGames.map(rawGameToGame)
 
 console.log(createEnum(genres, 'Genre'))
 console.log(createEnum(categories, 'Category'))
 
 const json = JSON.stringify(games)
 fs.writeFileSync('./data/games.json', json)
+
+function rawGameToGame(rawGame) {
+  return {
+    name: rawGame.name,
+    appId: rawGame.appId,
+    video: rawGame.video?.id,
+    categories:
+      rawGame.categories
+        .filter(({id}) => relevantCategories.includes(id))
+        .map(({id}) => id) ?? [],
+    genres: rawGame.genres?.map(({id}) => Number(id)) ?? [],
+    g2a: getG2a(rawGame.appId)
+  }
+}
 
 function createEnum(nameById, enumName) {
   const camelCased = _.mapValues(nameById, (key) =>
@@ -85,4 +74,26 @@ function getG2a(appId) {
     sellerVotes: _.sum(Object.values(offer?.customer?.votes)),
     sellerRating: offer?.customer?.rating
   }
+}
+
+function getCategoryIdMap(games) {
+  return _.mapValues(
+    _.keyBy(
+      _.flatten(games.map(({categories}) => categories)).filter(({id}) =>
+        relevantCategories.includes(id)
+      ),
+      'id'
+    ),
+    'description'
+  )
+}
+
+function getGenreIdMap(games) {
+  return _.mapValues(
+    _.keyBy(
+      _.flatten(selectedGames.map(({genres}) => genres)).filter((a) => a),
+      'id'
+    ),
+    'description'
+  )
 }
