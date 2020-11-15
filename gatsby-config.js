9b1799f3df332e0e6a1e231b1f1a74f960b13557
Finn Pauls <mbp@pauls.dev>
2020-11-15 14:42:56 +0000

    Use react helmet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-sitemap`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-react-helmet`
+  ],
   siteMetadata: {
     siteUrl: 'https://overwhelmingly-positive.com'
   }
