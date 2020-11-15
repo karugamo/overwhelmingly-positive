98c47e0891f042a822814d5d26dd81955bd1536c
lexenson <alexander.assmann91@gmail.com>
2020-11-15 11:57:51 +0100

    Add sitemap plugin to gatsby
    Creates sitemap on `npm run build` in /public folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`]
+  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-sitemap`],
+  siteMetadata: {
+    siteUrl: 'https://overwhelmingly-positive.com'
+  }
 }
