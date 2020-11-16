b0c2cd24f6ac26db9cbfa1683c5fe1b10cc343ae
Finn Pauls <mbp@pauls.dev>
2020-11-16 12:05:37 +0000

    Use ssh buildpack

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# add ssh key
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts 2> /dev/null
-cat "$ENV_DIR/SSH_KEY" > ~/.ssh/id_rsa
-echo >> ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-
 git clone git@github.com:flexiproducts/overwhelmingly-positive.git git-repo
 
 cp data/* git-repo/data
