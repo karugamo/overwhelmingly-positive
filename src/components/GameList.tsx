41d04cd42f46f26df5487f7afdde148840333358
Finn Pauls <mbp@pauls.dev>
2021-01-01 23:30:37 +0100

    Move ad to position 10

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -9,16 +9,18 @@ type GameListProps = {
   onSelectGame: (game: Game) => void
 }
 
+const adPosition = 10
+
 export default function GameList({games, onSelectGame}: GameListProps) {
-  const gamesBeforeAd = games.slice(0, 3)
-  const gamesAfterAd = games.slice(3)
+  const gamesBeforeAd = games.slice(0, adPosition - 1)
+  const gamesAfterAd = games.slice(adPosition - 1)
   return (
     <Container>
       {games.length === 0 && <Sorry>No games match your filters</Sorry>}
       {gamesBeforeAd.map((game) => (
         <GameThumbnail key={game.appId} game={game} onOpenGame={onSelectGame} />
       ))}
-      {games.length > 2 && <Amazon />}
+      {games.length >= adPosition && <Amazon />}
       {gamesAfterAd.map((game) => (
         <GameThumbnail key={game.appId} game={game} onOpenGame={onSelectGame} />
       ))}
